fc97f8c5137ad8d99d1a87c99f2c23aea2912b3b
mebinpdxc <105103168+mebinpdxc@users.noreply.github.com>
2022-05-07 16:00:44 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
 <?php
 $port = $_SERVER['WEBSITE_MYSQL_PORT']
-$con = new mysqli("127.0.0.1:$port","azure","6#vWHD_$","social_network");
+$con = mysqli_connect("127.0.0.1:$port","azure","6#vWHD_$","social_network");
 ?>
